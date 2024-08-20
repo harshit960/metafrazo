596992e7c03bcebacd3e2f264b9a15369e41a628
harshit960 <raj.harshit960@gmail.com>
2024-08-21 00:43:18 +0530

    chore: Add lazy loading for images in IgCard and Plates components

--- frontend/src/components/Plates.jsx
+++ frontend/src/components/Plates.jsx
@@ -4,13 +4,21 @@ import TiktokCard from './TiktokCard'
 import YoutubeCard from './YoutubeCard'
 
 function Plates() {
-  return (
-    <>
-    <IgCard/>
-    <TiktokCard/>
-    <YoutubeCard/>
-    </>
-  )
+    return (
+        <>
+            <div className="flex w-full  justify-center mt-14">
+                <div className="absolute transform -translate-x-72 z-20 -rotate-[6deg] -translate-y-8 hover:rotate-0 duration-200">
+                    <IgCard />
+                </div>
+                <div className="absolute transform -translate-x-0 z-0" >
+                    <TiktokCard />
+                </div>
+                <div className="absolute transform translate-x-72 z-20 -rotate-[6deg] -translate-y-16 hover:rotate-0 duration-200">
+                    <YoutubeCard />
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Plates
