26748b1bcd6321bc17e2ee590847b500295a7584
harshit960 <raj.harshit960@gmail.com>
2024-08-21 22:55:03 +0530

    refactor: Update Plates component layout and styling

--- frontend/src/components/Plates.jsx
+++ frontend/src/components/Plates.jsx
@@ -6,14 +6,14 @@ import YoutubeCard from './YoutubeCard'
 function Plates() {
     return (
         <>
-            <div className="flex w-full flex-wrap justify-center mt-14">
-                <div className="lg:absolute  lg:transform lg:-translate-x-72 z-20 -rotate-[6deg] -translate-y-8 hover:rotate-0 duration-200">
+            <div className="flex w-full flex-wrap justify-center lg:mt-14">
+                <div className="lg:absolute scale-75 lg:scale-100 scal lg:transform lg:-translate-x-72 z-20 -rotate-[6deg] lg:-translate-y-8 hover:rotate-0 duration-200">
                     <IgCard />
                 </div>
-                <div className="lg:absolute transform lg:-translate-x-0 z-0" >
+                <div className="lg:absolute scale-75 lg:scale-100 transform lg:-translate-x-0 z-0" >
                     <TiktokCard />
                 </div>
-                <div className="lg:absolute transform lg:translate-x-72 z-20 lg:-rotate-[6deg] rotate-[6deg] lg:-translate-y-16 translate-y-10 hover:rotate-0 duration-200">
+                <div className="lg:absolute scale-75 lg:scale-100 transform lg:translate-x-72 z-20 lg:-rotate-[6deg] rotate-[6deg] lg:-translate-y-16 translate-y-0 hover:rotate-0 duration-200">
                     <YoutubeCard />
                 </div>
             </div>
