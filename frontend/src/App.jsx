6ddfb54fedc04eeb16e10a466e002d17b826b9be
harshit960 <raj.harshit960@gmail.com>
2024-09-01 01:57:40 +0530

    refactor: Update CTABtn component styling and add hover effect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,34 @@
-import Hero from "./section/Hero";
-import Products from "./section/Products";
+import Benefits from "./sections/Benefits";
+import CTA from "./sections/CTA";
+import Email from "./sections/Email";
+import FAQ from "./sections/FAQ";
+import Features from "./sections/Features";
+import Footer from "./sections/Footer";
+import Guide from "./sections/Guide";
+import Hero from "./sections/Hero";
+import PreTest from "./sections/PreTest";
+import Prices from "./sections/Prices";
+import Reviews from "./sections/Reviews";
+import ShowReel from "./sections/ShowReel";
+import Testimonials from "./sections/Testimonials";
 
 export default function App() {
   return (
     <>
     <Hero/>
-    <Products/>
-    
+    <ShowReel/>
+    <PreTest/>
+    <Testimonials/>
+    <Features/>
+    <Guide/>
+    <Benefits/>
+    <Reviews/>
+    <Prices/>
+    <div className="h-[50vh] w-full bg-[#F0F3F6]"></div>
+    <FAQ/>
+    <Email/>
+    <CTA/>
+    <Footer/>
     </>
   )
 }
---
+++ frontend/src/sections/Hero.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+import CompniesLogo from '../components/CompniesLogo'
+import CTABtn from '../components/CTABtn'
+
+function Hero() {
+    return (
+        <>
+            <div className=" w-full h-screen justify-start flex flex-col items-center py-[32px] overflow-hidden ">
+                <img src="assets/grid.png" alt="" srcset="" className='absolute -z-10 top-0 left-0 max-w-full  ' />
+                <div className="bg-[#F0F3F6] -z-20 w-full h-screen absolute top-0 left-0"></div>
+                <NavBar />
+
+                <div className=" text-center self-center justify-self-center my-auto z-10 w-full">
+                    <div className="font-satoshi-lightItalic text-[56px] leading-[67px] font-bold">
+                        AI Dubbing and Translation <br />
+                        for <span className='text-[#0A47F2]'>Educators</span>
+                    </div>
+                    <div className="text-[20px] leading-[30px] font-normal pt-[24px]">
+                        Globalize your content with lip-sync, preserved original voices, and accurate <br /> on-screen text translation—faster and more cost-effectively than ever
+                    </div>
+                    <div className="mt-[14px]">
+                            <CTABtn/>
+                        <div className="text-[16px] mt-2">
+                        Learn More
+                        </div>
+                    </div>
+                    <div className="flex-col flex items-center justify-center mt-[48px] ">
+
+                        <div className="text-[16px] mt-8">
+                        Helping people translate videos at
+                        </div>
+                        <CompniesLogo/>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Hero
