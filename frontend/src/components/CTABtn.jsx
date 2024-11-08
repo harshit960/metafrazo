2b8527e16e299157be84f5072aca92f29923774c
harshit960 <raj.harshit960@gmail.com>
2024-11-08 11:18:03 +0530

    refactor: Improve CTABtn styling and update CTA section layout for better responsiveness

--- frontend/src/components/CTABtn.jsx
+++ frontend/src/components/CTABtn.jsx
@@ -4,7 +4,7 @@ function CTABtn() {
     return (
         <>
             <button 
-            className="bg-[#0A47F2] button font-satoshi text-[18px] font-bold text-[#fff] py-[10px] px-[25px] rounded-[16px] mt-[30px] ">
+            className=" button font-satoshi text-[18px] font-bold text-[#fff] py-[10px] px-[25px] duration-200 mt-[30px] ">
                 
                 Try for free
                 </button>
--- frontend/src/sections/CTA.jsx
+++ frontend/src/sections/CTA.jsx
@@ -15,7 +15,7 @@ function CTA() {
                 <div className="lg:text-[56px] text-[40px] z-20 px-4 font-bold my-6 text-center leading-[67px] font-satoshi">Unlock the power of AI to translate <br /> your video materials</div>
                 <div className="lg:text-[20px] text-[14px] px-12 z-20 text-center font-satoshiMed">
                     Experience the video translation tool you'll love. It's the only one you'll ever need.                </div>
-                <div className="mt-6 z-20">
+                <div className="mt-6 z-20 ">
                     <CTABtn />
                 </div>
                 {/* <img src="assets/BG.png" alt="" srcset="" className='absolute max-h-full w-full' /> */}
