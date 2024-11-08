f9f501e0e6b96ddabf7294f3d56f70ba5dd08990
harshit960 <raj.harshit960@gmail.com>
2024-11-08 14:54:59 +0530

    refactor: Add images to ReviewCard component for enhanced visual appeal

--- frontend/src/components/ReviewCard.jsx
+++ frontend/src/components/ReviewCard.jsx
@@ -9,7 +9,9 @@ function ReviewCard(props) {
                     {props.review}
                 </div>
                 <div className="flex items-center mt-3">
-                    <div className="bg-black rounded-full w-[48px] h-[48px]"></div>
+                    <div className="bg-black rounded-full w-[48px] h-[48px]">
+                        <img src={props.img} alt="" srcset="" className="rounded-full w-[48px] h-[48px]"/>
+                    </div>
                     <div className="flex flex-col mx-2">
                         <div className="text-[18px]  font-satoshiMed">{props.name}</div>
                         <div className="text-[18px] text-[#0A47F2] font-satoshiMed">{props.position}</div>
